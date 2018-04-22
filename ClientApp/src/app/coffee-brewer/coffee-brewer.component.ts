9ae647a049523f1632cbb8b0e52ddaaecf5cab99
Tommy Forsberg <tommy.forsberg@hotmail.com>
2018-04-22 21:57:20 +0200

    Add project files.

---
+++ ClientApp/src/app/coffee-brewer/coffee-brewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-brewer',
+  templateUrl: './coffee-brewer.component.html',
+  styleUrls: ['./coffee-brewer.component.css']
+})
+export class CoffeeBrewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
